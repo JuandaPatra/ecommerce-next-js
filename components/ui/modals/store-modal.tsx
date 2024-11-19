52761ba74644d508f694d420cbb25b425f195356
R. Juanda Patra <patrajuanda10@gmail.com>
2024-11-19 07:01:50 +0700

    add dialogue form input

--- components/ui/modals/store-modal.tsx
+++ components/ui/modals/store-modal.tsx
@@ -1,26 +1,63 @@
-'use Client'
+"use Client";
 
-import { useStoreModalStore } from "@/hooks/use-store-modal"
-import { Modal } from "../modal"
+import { useStoreModalStore } from "@/hooks/use-store-modal";
+import { Modal } from "../modal";
 
-import * as z from 'zod';
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField, FormItem, FormLabel } from "../form";
+import { Input } from "../input";
+import { Button } from "../button";
 
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
-// const formSchema =z.object({
-//     name:   
-// })
+const onSubmit = async (values: z.infer<typeof formSchema>) => {
+  console.log(values);
+};
 
 export const StoreModal = () => {
-    const storeModal = useStoreModalStore();
-    return (
-        <Modal 
-        title="Buat Store"
-        description="Lengkapi data store anda"
-        isOpen={storeModal.isOpen}
-        onClose={storeModal.onClose}
-        >
-            Store Form
+  const storeModal = useStoreModalStore();
 
-        </Modal>
-    )
-}
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  return (
+    <Modal
+      title="Buat Store"
+      description="Lengkapi data store anda"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div>
+          <Form {...form}>
+            <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <Input placeholder="Nama Toko" {...field} />
+                  </FormItem>
+                )}
+              />
+              <div className="px-3 pt-3 space-x-2 flex justify-end w-full">
+                <Button variant={"outline"} onClick={storeModal.onClose}>Cancel</Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  );
+};
